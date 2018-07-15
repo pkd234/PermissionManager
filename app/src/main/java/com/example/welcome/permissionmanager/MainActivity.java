36d71c66e01b971371106caf8d4a21ed8ac4a302
PANKAJ KUMAR <pkd.234@gmail.com>
2018-07-15 18:38:03 +0530

    Permission Manager Class Added
    Added the basic features that needed for Wokring of Library:
    Added Class :+1: Permission Mangaer with will perform the basic funtionaliry of the library

--- app/src/main/java/com/example/welcome/permissionmanager/MainActivity.java
+++ app/src/main/java/com/example/welcome/permissionmanager/MainActivity.java
@@ -1,16 +1,36 @@
 package com.example.welcome.permissionmanager;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.permissionmanagerandroid.PermissionManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] permissions={
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.CALL_PHONE
+    };
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PermissionManager.Testing("demo",getApplicationContext());
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PermissionManager.requestPermission(MainActivity.this,permissions,true);
+
+
+            }
+        });
+
     }
 }
--- permissionmanagerandroid/src/main/java/com/example/permissionmanagerandroid/PermissionManager.java
+++ permissionmanagerandroid/src/main/java/com/example/permissionmanagerandroid/PermissionManager.java
@@ -1,10 +1,33 @@
 package com.example.permissionmanagerandroid;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.widget.Toast;
 
+import java.security.Permission;
+
 public class PermissionManager {
-    public static void Testing(String s, Context context){
-        Toast.makeText(context, ""+s, Toast.LENGTH_SHORT).show();
+    private static final String TAG = "PermissionManager";
+
+    public static void requestPermission(Context context,String[] Permissoins,Boolean RR){
+        int[] x=new int[Permissoins.length];
+        for(int i=0;i<Permissoins.length;i++){
+            x[i]= ActivityCompat.checkSelfPermission(context, Permissoins[i]);
+
+        }
+        for(int i=0;i<x.length;i++){
+            Activity activity = (Activity) context;
+            if(x[i]!= PackageManager.PERMISSION_GRANTED){
+                Log.d(TAG, "requestPermission: "+ Permissoins[i]);
+                ActivityCompat.requestPermissions(activity, Permissoins, 1);
+
+
+            }
+        }
     }
+
 }
